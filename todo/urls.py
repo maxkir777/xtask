f9401a7143de0f4960700fd8128c250bea166c4e
skair72 <evenko100500@gmail.com>
2019-05-06 05:40:49 +0500

    Small changes

--- todo/urls.py
+++ todo/urls.py
@@ -1,16 +1,18 @@
-from django.urls import include, path
+from django.urls import path
 from rest_framework.routers import DefaultRouter
-
+from rest_framework_simplejwt import views as jwt_views
 from todo.views import BoardViewSet, TaskViewSet
 from . import views
 
 router = DefaultRouter()
 router.register(r'boards', BoardViewSet, basename='attractions')
-router.register(r'task', TaskViewSet, basename='categories')
+router.register(r'tasks', TaskViewSet, basename='categories')
 urlpatterns = router.urls
 
 extended_urlpatterns = [
     path('', views.UserListView.as_view()),
+    path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 urlpatterns.extend(extended_urlpatterns)

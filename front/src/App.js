0203f70d03d660a2a1775e3080f3a875e84092fa
skair72 <evenko100500@gmail.com>
2019-05-13 02:30:46 +0500

    add redirecting

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route } from 'react-router-dom'
 import SignIn from './components/SignIn/SignInContainer'
+import Boards from './components/Boards/BoardsContainer'
 
 const App = () => (
   <div>
     <main>
       <Route exact path="/" component={SignIn} />
+      <Route exact path="/boards" component={Boards}/>
     </main>
   </div>
 );
---
+++ front/src/components/Boards/Boards.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Boards extends React.Component{
+  render() {
+    return (<p>Boards</p>)
+  }
+}
+
+export default Boards
